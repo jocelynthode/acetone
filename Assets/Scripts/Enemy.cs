c0f647f2ff93f90cc7261be544cc2afa1632de3e
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-03-16 14:20:10 +0100

    add basic AI for enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -18,15 +18,29 @@ public class Enemy : MovingObject {
            
 	}
 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!base.turn) return;
+        int xDir = 0;
+        int yDir = 0;
+        float randomValue = UnityEngine.Random.value;
+        if (randomValue < 0.5f)
+        {
+            xDir = UnityEngine.Random.value < 0.5f ? -1 : 1; ;
+        } else
+        {
+            yDir = UnityEngine.Random.value < 0.5f ? -1 : 1;
+        }
+        AttemptMove<Player>(xDir, yDir);
+
+    }
+
     protected override void AttemptMove<T>(int xDir, int yDir)
     {
-        if(skipMove)
-        {
-            skipMove = false;
-            return;
-        }
+        base.turn = false;
         base.AttemptMove<T>(xDir, yDir);
-        skipMove = true;
     }
 
     public void MoveEnemy()
@@ -39,6 +53,6 @@ public class Enemy : MovingObject {
 
     protected override void OnCantMove<T>(T component)
     {
-        
+        base.turn = true;
     }
 }
