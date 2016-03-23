c112fb1da2a8c15aeed364b324a6e905e5fd269d
gravelpyt <pythoudg@gmail.com>
2016-03-23 10:05:08 +0100

    get damage in moving object

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -13,6 +13,7 @@ namespace Completed
         private Rigidbody2D rb2D;               //The Rigidbody2D component attached to this object.
         private float inverseMoveTime;          //Used to make movement more efficient.
 
+        public int Hp;
         //Protected, virtual functions can be overridden by inheriting classes.
         protected virtual void Start()
         {
@@ -118,5 +119,10 @@ namespace Completed
         //OnCantMove will be overriden by functions in the inheriting classes.
         protected abstract void OnCantMove<T>(T component)
             where T : Component;
+
+        protected void takeDamage(int att, int def)
+        {
+            Hp = Hp - att * def;
+        }
     }
 }
