3ba57c945b7f812edf06213d886031c51d6fea2f
Simon <simon@brulhart.me>
2016-04-01 13:14:54 +0200

    Refactor UpgradeMenu to take cost in account

--- Assets/Menus/UpgradeMenu.cs
+++ Assets/Menus/UpgradeMenu.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class UpgradeMenu : MonoBehaviour {
-    // string[] upgrades = new string[] { "maxHealth", "attack", "defense", "viewers" };
-    string[] upgrades = new string[] { "maxHealth", "attack" };
+
+    Dictionary<string, Upgrade> upgrades;
+
     // Use this for initialization
     void Awake () {
-        foreach (string up in upgrades)
+        RefreshMenu();
+    }
+
+    void RefreshMenu()
+    {
+        // Pre-compute all upgrades
+        upgrades = new Dictionary<string, Upgrade>();
+        foreach (var item in Upgrade.upgradeFunctions)
         {
-            GameObject.Find(up).GetComponent<Text>().text = PlayerPrefs.GetInt(up).ToString();
-        } 
+            int stat = PlayerPrefs.GetInt(item.Key);
+            int level = PlayerPrefs.GetInt(item.Key + "Level");
+            Upgrade upgrade = item.Value(level, stat);
+            upgrades.Add(item.Key, upgrade);
+
+            GameObject.Find(item.Key).GetComponent<Text>().text = PlayerPrefs.GetInt(item.Key).ToString();
+            // TODO: Set cost
+        }
+
         GameObject.Find("moneyText").GetComponent<Text>().text = string.Format("Money: {0:C2}", PlayerPrefs.GetInt("money"));
     }
 	
@@ -28,16 +44,49 @@ public class UpgradeMenu : MonoBehaviour {
     {
         PlayerPrefs.DeleteAll();
         GameManager.CheckPlayerPrefs(true);
-        foreach (string up in upgrades)
-            GameObject.Find(up).GetComponent<Text>().text = PlayerPrefs.GetInt(up).ToString();
+        RefreshMenu();
     }
-    public void UpdatePrefs(string Name)
+
+    public void UpdatePrefs(string name)
     {
-        Text updateText;
-        updateText = GameObject.Find(Name).GetComponent<Text>();
-        PlayerPrefs.SetInt(Name, PlayerPrefs.GetInt(Name)+10);
-        updateText.text = PlayerPrefs.GetInt(Name).ToString();
+        int level = PlayerPrefs.GetInt(name + "Level");
+        Upgrade upgrade = upgrades[name];
+        int money = PlayerPrefs.GetInt("money");
+        int newMoney = money - upgrade.Cost;
+
+        if (newMoney < 0)
+            return;
+        PlayerPrefs.SetInt("money", newMoney);
+        PlayerPrefs.SetInt(name, upgrade.Stat);
+        PlayerPrefs.SetInt(name + "Level", level + 1);
         PlayerPrefs.Save();
+
+        RefreshMenu();
+    }
+
+
+    public class Upgrade
+    {
+        public int Cost { get; private set; }
+        public int Stat { get; private set; }
+        public Upgrade(int cost, int stat)
+        {
+            Cost = cost;
+            Stat = stat;
+        }
+
+        public delegate Upgrade UpgradeFunc(int baseLevel, int baseStat);
+        public static Dictionary<string, UpgradeFunc> upgradeFunctions;
+
+        static Upgrade() {
+            upgradeFunctions = new Dictionary<string, UpgradeFunc>();
+            upgradeFunctions.Add("maxHealth", (baseLevel, baseStat) =>
+                new Upgrade((baseLevel + 1) * 10, baseStat + 10)
+            );
+            upgradeFunctions.Add("attack", (baseLevel, baseStat) =>
+                new Upgrade((baseLevel + 1) * 10, baseStat + 10)
+            );
+        }
     }
 }
 
