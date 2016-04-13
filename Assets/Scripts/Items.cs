7bd07f358df650dc1bcd5a6775ff3cc8a79a951b
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-04-13 16:27:55 +0200

    Add base structure of Item

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class Items : MonoBehaviour {
+    public enum ItemType
+    {
+        AD,
+        BOMB,
+        VIEWBOT
+    }
+
+    public static void useItem(ItemType item) 
+    {
+        switch (item)
+        {
+            case ItemType.AD:
+                useAd();
+                break;
+            case ItemType.BOMB:
+                useBomb();
+                break;
+            case ItemType.VIEWBOT:
+                useViewbot();
+                break;
+            default:
+                throw new System.ArgumentException();
+        }
+    }
+
+    static void useAd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    static void useBomb()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    static void useViewbot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
