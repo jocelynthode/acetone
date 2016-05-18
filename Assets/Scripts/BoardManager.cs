e566052dfdd44df994d7357ba36a084b1b1151ea
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-05-18 18:20:18 +0200

    Fix Level generation
    Now level generation works with multiple possible exits

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -12,7 +12,7 @@ public class BoardManager : MonoBehaviour
 
 
     //Lower and upper limit for our random number of inner walls and items
-    private Range wallCount = new Range(20, 20);
+    private Range wallCount = new Range(80, 100);
     private Range itemAdCount = new Range(0, 1, 8);
     private Range itemBombCount = new Range(0, 1, 4);
     private Range itemViewbotCount = new Range(0, 1, 8);
@@ -25,34 +25,59 @@ public class BoardManager : MonoBehaviour
     public GameObject[] floorTiles;                                  //Array of floor prefabs.
     public GameObject[] enemyTiles;                                 //Array of enemy prefabs.
     public GameObject[] outerWallTiles;                             //Array of outer tile prefabs.
+    public Vector2 exit;
 
     public Player player;
     public List<Enemy> enemies;
 
     private Transform boardHolder;                                  //A variable to store a reference to the transform of our Board object.
-    private List<Vector3> gridPositions = new List<Vector3>();   //A list of possible locations to place tiles.
-    private List<Vector3> path = new List<Vector3>();
+    private List<Vector2> gridPositions = new List<Vector2>();   //A list of possible locations to place tiles.
+    private List<Vector2> path = new List<Vector2>();
 
     //Clears our list gridPositions and prepares it to generate a new board.
 
     void initalisePath()
     {
         path.Clear();
-        List<Vector3> vectors = new List<Vector3>();
-        for (int i = 0; i < columns - 1; i++)
-            vectors.Add(new Vector3(1, 0, 0f));
-        for (int i = 0; i < rows - 1; i++)
-            vectors.Add(new Vector3(0, 1, 0f));
+        List<Vector2> vectors = new List<Vector2>();
+        int playerX = 12;
+        int playerY = 7;
+        int xDir = 1;
+        int yDir = 1;
 
-        Vector3 origine = new Vector3(0, 0, 0f);
-        path.Add(origine);
+
+        //select one exit
+        float exitX = UnityEngine.Random.value;
+        float exitY = UnityEngine.Random.value;
+        if (exitX < 0.5)
+            exitX = 0;
+        else
+            exitX = columns -1 ;
+        if (exitY < 0.5)
+            exitY = 0;
+        else
+            exitY = rows - 1;
+        
+        exit = new Vector2(exitX, exitY);
+        Vector2 playerPosition = new Vector2(playerX, playerY);
+        path.Add(playerPosition);
+        xDir = (playerPosition.x < exit.x) ? 1 : -1;
+        yDir = (playerPosition.y < exit.y) ? 1 : -1;
+
+        int deltaX = (int) Math.Abs(playerPosition.x - exit.x);
+        int deltaY = (int) Math.Abs(playerPosition.y - exit.y);
+
+        for (int i = 0; i < deltaX; i++)
+            vectors.Add(new Vector2(xDir, 0));
+        for (int i = 0; i < deltaY; i++)
+            vectors.Add(new Vector2(0, yDir));
+        
         while (vectors.Count > 0)
         {
             int i = Random.Range(0, vectors.Count);
-            origine = origine + vectors[i];
+            playerPosition = playerPosition + vectors[i];
             vectors.Remove(vectors[i]);
-            path.Add(origine);
-
+            path.Add(playerPosition);
         }
 
     }
@@ -68,8 +93,8 @@ public class BoardManager : MonoBehaviour
             //Within each column, loop through y axis (rows).
             for (int y = 0; y < rows; y++)
             {
-                //At each index add a new Vector3 to our list with the x and y coordinates of that position.
-                Vector3 temp = new Vector3(x, y, 0f);
+                //At each index add a new Vector2 to our list with the x and y coordinates of that position.
+                Vector2 temp = new Vector2(x, y);
                 if (!path.Contains(temp))
                     gridPositions.Add(temp);
             }
@@ -98,9 +123,9 @@ public class BoardManager : MonoBehaviour
                 if (x == -1 || x == columns || y == -1 || y == rows)
                     toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
 
-                //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector3 corresponding to current grid position in loop, cast it to GameObject.
+                //Instantiate the GameObject instance using the prefab chosen for toInstantiate at the Vector2 corresponding to current grid position in loop, cast it to GameObject.
                 GameObject instance =
-                    Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+                    Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity) as GameObject;
 
                 //Set the parent of our newly instantiated object instance to boardHolder, this is just organizational to avoid cluttering hierarchy.
                 instance.transform.SetParent(boardHolder);
@@ -111,18 +136,18 @@ public class BoardManager : MonoBehaviour
 
 
     //RandomPosition returns a random position from our list gridPositions.
-    Vector3 RandomPosition()
+    Vector2 RandomPosition()
     {
         //Declare an integer randomIndex, set it's value to a random number between 0 and the count of items in our List gridPositions.
         int randomIndex = Random.Range(0, gridPositions.Count);
 
-        //Declare a variable of type Vector3 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
-        Vector3 randomPosition = gridPositions[randomIndex];
+        //Declare a variable of type Vector2 called randomPosition, set it's value to the entry at randomIndex from our List gridPositions.
+        Vector2 randomPosition = gridPositions[randomIndex];
 
         //Remove the entry at randomIndex from the list so that it can't be re-used.
         gridPositions.RemoveAt(randomIndex);
 
-        //Return the randomly selected Vector3 position.
+        //Return the randomly selected Vector2 position.
         return randomPosition;
     }
 
@@ -143,8 +168,8 @@ public class BoardManager : MonoBehaviour
         List<GameObject> objects = new List<GameObject>();
         for (int i = 0; i < objectCount; i++)
         {
-            //Choose a position for randomPosition by getting a random position from our list of available Vector3s stored in gridPosition
-            Vector3 randomPosition = RandomPosition();
+            //Choose a position for randomPosition by getting a random position from our list of available Vector2s stored in gridPosition
+            Vector2 randomPosition = RandomPosition();
 
             //Choose a random tile from tileArray and assign it to tileChoice
             GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
@@ -187,10 +212,10 @@ public class BoardManager : MonoBehaviour
         });
 
         //Instantiate the exit tile in the upper right hand corner of our game board
-        Instantiate(exitTile, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity);
+        Instantiate(exitTile, exit, Quaternion.identity);
 
         //Instantiate the player tile in the bottom left hand corner
-        //GameObject playerObject = Instantiate(playerTile, new Vector3(0, 0, 0f), Quaternion.identity) as GameObject;
+        //GameObject playerObject = Instantiate(playerTile, new Vector2(0, 0, 0f), Quaternion.identity) as GameObject;
         //player = playerObject.GetComponent<Player>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
