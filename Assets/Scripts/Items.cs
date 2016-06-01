9933baf0de213eb2f11acd1185fdf67091e21b92
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-06-01 10:37:24 +0200

    Add sound when using a ladder

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -58,6 +58,7 @@ public static class Items
         {
             GameManager.instance.boardScript.enemies[0].Die();
             i = i - 1;
+            // http://audiosoundclips.com/8-bit-explosion-blast-sound-effects-sfx/
             GameObject.Find("BombExplosion").GetComponent<AudioSource>().Play();
         }
     }
@@ -69,7 +70,9 @@ public static class Items
 
     static void useLadder()
     {
+        GameObject.Find("LadderUseSound").GetComponent<AudioSource>().Play();
         GameManager.instance.OnLevelCompletion(PlayerPrefs.GetInt("itemsPowerLevel")*2+2);
+
     }
 
 }
